114f8d10be6b69367fa0d0483ec1b9c76ed7144e
Albert Hattingh <albert.h0504@gmail.com>
2020-10-05 13:40:23 +0200

    Table text alignment

--- src/components/content3.js
+++ src/components/content3.js
@@ -15,7 +15,8 @@ function Content3(props) {
         const languages = stats['ranks']['languages'];
 
         for (var lang in languages) {
-            rows.push(<tr><td>{lang.charAt(0).toUpperCase() + lang.slice(1)}</td><td>{languages[lang]["name"]}</td></tr>);
+            rows.push(<tr><td>{lang.charAt(0).toUpperCase() + lang.slice(1)}</td>
+                <td className="rank">{languages[lang]["name"]}</td></tr>);
         }
     }
 
@@ -27,13 +28,15 @@ function Content3(props) {
                     In my free time I enjoy doing coding challenges on Codewars.
                     See below for a list of languages I am proficient in, and my corresponding Codewars rank.
                 </p>
-                <img id="c-rank"
-                     src="https://www.codewars.com/users/alberthattingh/badges/micro"
-                     alt="My Codewars rank"
-                     onClick={onRankClick}/>
-                 <table>
-                     {rows}
-                 </table>
+                <div className="center-box">
+                    <img id="c-rank"
+                         src="https://www.codewars.com/users/alberthattingh/badges/micro"
+                         alt="My Codewars rank"
+                         onClick={onRankClick}/>
+                    <table>
+                        {rows}
+                    </table>
+                </div>
             </section>
             <img src="https://img.icons8.com/ios/100/000000/circled-chevron-right.png"
                  className="nextButton"
